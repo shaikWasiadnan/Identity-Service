263a6dd3a6584736d11af5ae399a0db5e03a28a5
Adnan <skadnan947@gmail.com>
2025-11-27 15:54:08 +0530

    Created identity Service

---
+++ src/main/java/com/TalentSphere/identity_service/entities/User.java
@@ -0,0 +1,21 @@
+package com.TalentSphere.identity_service.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+}
---
+++ src/main/java/com/TalentSphere/identity_service/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.TalentSphere.identity_service.repository;
+
+import com.TalentSphere.identity_service.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+}
